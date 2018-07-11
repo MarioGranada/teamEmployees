1433ccd13c737f95276a4c345fc9aae89e47f698
Mario Granada <mgranada@conversantmedia.com>
2018-07-10 23:56:25 -0500

    store__global_reducer

---
+++ src/app/store/reducers.ts
@@ -0,0 +1,8 @@
+import { ActionReducerMap } from '@ngrx/store';
+import { AppState } from './state';
+
+import { reducer as employee } from './employee/employee.reducer';
+
+export const reducers: ActionReducerMap<AppState> = {
+  employee
+};
